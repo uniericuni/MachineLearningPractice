6e863531fb117ee122efde5e336e60e43ff98f69
Uniericuni <uniericuni@gmail.com>
2016-09-27 23:53:41 -0400

    [Backup] WIP

--- LR2.py
+++ LR2.py
@@ -25,40 +25,25 @@ def gradFunc(x, y, theta, la):
 
     dim, n = x.shape
     ret = np.zeros(dim+1)
-    X = np.ones((dim+1,n))
-    X[1:,:] = x
-    S = sigmoid( -np.dot(np.transpose(X), theta) )
-    return np.dot(X, np.transpose(y-S)) + 2*la*theta 
-'''    
-for i in range(0,n):
-        x_bar[:] = x[:,i]
-        t = -np.dot(x_bar, theta)
-        ret += x_bar * (y[:,i]-sigmoid(t))
+    x_bar = np.ones(dim+1)
+    for i in range(0,n):
+        x_bar[1:] = x[:,i]
+        t = np.dot(x_bar, theta)
+        ret += - x_bar * y[:,i] * sigmoid(y[:,i]*t)
     ret += 2*la*theta
  
     return ret
-'''
 
 # hessian of the objective function
 def hessFunc(x, y, theta, la):
 
     dim, n = x.shape
     ret = np.zeros((dim+1, dim+1))
-    X = np.ones((dim+1,n))
-    X[1:,:] = x
-    St = sigmoid( -np.dot(np.transpose(X), theta) )
-    S = np.diagflat(St)
-    s1 = np.dot(X, S)
-    s2 = np.dot((1-S), np.transpose(X))
-    return np.dot(s1,s2)-np.eye(dim+1)*2*la
-'''
-x_bar = np.ones(dim+1)
+    x_bar = np.ones(dim+1)
     for i in range(0,n):
         x_bar[1:] = x[:,i]
-        t = -np.dot(x_bar, theta)
-        ret += sigmoid(t)*(1-sigmoid(t))*np.outer(x_bar,x_bar)        
-    ret += 2*la
-    print ret
+        t = np.dot(x_bar, theta)
+        ret += sigmoid(y[:,i]*t) * (1-sigmoid(y[:,i]*t)) * np.outer(x_bar,x_bar)
+    ret += 2*la*np.eye(dim+1)
 
     return ret
-'''
--- hw2_2.py
+++ hw2_2.py
@@ -5,10 +5,9 @@ import scipy.io as sio
 # x: feature vectors
 # y: labels
 #
- 
 
 import matplotlib.pyplot as plt
-import LR as lr
+import LR2 as lr
 
 # training env setting
 trainingNum = 2000
@@ -17,26 +16,39 @@ la = 10
 mnist_49_3000 = sio.loadmat('mnist_49_3000.mat')    # import data
 x = mnist_49_3000['x']
 y = mnist_49_3000['y']
+dim, n = x.shape
+
+# modify label
+# for i in range(0,n):
+#     if y[:,i] == [-1]:    
+#         y[:,i] = [0] 
 
 # seperating training and testing data
-dim, n = x.shape
-xT = x[:,0:trainingNum-1]
-yT = y[:,0:trainingNum-1]
+xT = x[:,0:trainingNum]
+dim, n = xT.shape
+yT = y[:,0:trainingNum]
 xTest = x[:,trainingNum:n-1]
 yTest = y[:,trainingNum:n-1]
-dim, n = xT.shape
 
+print(str(trainingNum))
+theta = np.zeros(dim+1)  # training offset
+H=lr.hessFunc(xT,yT,theta,la)
+print(H)
+'''
 # Newton optimization
 it = 0
 error = float('Inf')
-theta = np.ones(dim+1)  # training offset
-while error > e
+theta = np.zeros(dim+1)  # training offset
+while error > e:
     it += 1
-    G = lr.gradFunc(x, y, theta, la)
-    H = lr.hessFunc(x, y, theta, la)
-    theta -= inv(H) * G
+    G = lr.gradFunc(xT, yT, theta, la)
+    print G
+    H = lr.hessFunc(xT, yT, theta, la)
+    print H
+    theta -= np.linalg.inv(H) * G
     error = lr.oFunc(x, y, theta, la)
     print('error: %s | iteration: %s', str(error), str(it))
 
 print('error: %s | iteration: %s', str(error), str(it))
 print('theta: ' + theta)
+'''
